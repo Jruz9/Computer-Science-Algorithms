38f3d5793a1b2c68530c2c104dfd4fbb5e4bbd83
jruz9 <josec300@live.com>
2020-12-16 13:32:13 -0500

    changed name to a file and completed DepthFirstPaths algorithm

--- Algorithms/src/main/java/Graphs/DepthFirstPaths.java
+++ Algorithms/src/main/java/Graphs/DepthFirstPaths.java
@@ -1,4 +1,42 @@
 package Graphs;
 
+import java.util.Stack;
+
 public class DepthFirstPaths {
+    private boolean[] marked;
+    private  int[] edgeTo;
+    private final int s;
+    public DepthFirstPaths(Graph graph, int s)
+    {
+        marked=new boolean[graph.v()];
+        edgeTo= new int[graph.E()];
+        this.s=s;
+        dfs(graph,s);
+    }
+    private void dfs(Graph g, int v)
+    {
+        marked[v]=true;
+        for (int w: g.adj(v)){
+            if ((!marked[w]))
+            {
+                edgeTo[w]=v;
+                dfs(g,w);
+            }
+        }
+    }
+    public boolean hasPathTo(int v){
+        return marked[v];
+    }
+    public  Iterable<Integer> pathTo(int v){
+        if(!hasPathTo(v)){
+            return  null;
+        }
+        Stack<Integer> path=new Stack<Integer>();
+
+        for(int x=v;x !=s;x=edgeTo[x]){
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
 }

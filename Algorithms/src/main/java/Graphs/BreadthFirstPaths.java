ad4aaa86ba92b61c532fb10d80c88c178c2f9a45
jruz9 <josec300@live.com>
2020-12-16 14:53:04 -0500

    finished BreathFirstPaths algorithm

--- Algorithms/src/main/java/Graphs/BreadthFirstPaths.java
+++ Algorithms/src/main/java/Graphs/BreadthFirstPaths.java
@@ -1,4 +1,50 @@
 package Graphs;
 
+import fundamentals.Queue;
+
+import java.util.Stack;
+
 public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph graph,int s){
+        marked =new boolean[graph.v()];
+        edgeTo= new int[graph.v()];
+        this.s=s;
+        bfs(graph,s);
+    }
+    private void bfs(Graph G,int s){
+        Queue<Integer>queue=new Queue<Integer>();
+        marked[s]=true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()){
+            int v=queue.dequeue();
+            for(int w: G.adj(v)){
+                if(!marked[w]){
+                    edgeTo[w]=v;
+                    marked[w]=true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+    public boolean hasPathTo(int v){
+        return marked[v];
+    }
+    public Iterable<Integer>pathTo(int v){
+        if (!hasPathTo(v)){
+            return null;
+        }
+
+        Stack<Integer> path=new Stack<Integer>();
+
+        for (int x=v; x !=s; x=edgeTo[x]){
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+
 }

3ad69a13cd59b7f7358748657aec0f328bf24b07
jruz9 <josec300@live.com>
2021-01-14 18:20:05 -0500

    added some comments to understand the progam betterand changed some terrible variable names lol

--- Algorithms/src/main/java/Graphs/Graph.java
+++ Algorithms/src/main/java/Graphs/Graph.java
@@ -5,23 +5,27 @@ import java.io.DataInputStream;
 import java.io.IOException;
 public class Graph {
     private final int  numberOfVertices;    //number of vertices
-    private int numberOfEdges;
-    private Bag<Integer>[] adjacentLists;
+    private int numberOfEdges;  //number of edges
+    private Bag<Integer>[] adjacentLists;   //adjacent list
+
+
+    ////make the the bag (graph)
     public Graph(int  numberOfVertices){
-        this.numberOfVertices=numberOfVertices;
-        adjacentLists=(Bag<Integer>[]) new Bag[numberOfVertices];
+        this.numberOfVertices=numberOfVertices; //make the private variable link to the one in the argument
+        adjacentLists=(Bag<Integer>[]) new Bag[numberOfVertices];   //initializes all lists in the bag
         for(int vertices=0;vertices<numberOfVertices;vertices++){
             adjacentLists[vertices]=new Bag<Integer>();// creates a list for each part of the bag adt
 
         }
     }
-    public Graph(DataInputStream in) throws IOException {
-        this(in.readInt());
-        int E= in.readInt();
-        for (int i =0;i<E;i++){
-            int v= in.readInt();
-            int w= in.readInt();
-            addEdge(v,w);
+    //this method add vertex and edges to connect each list together in the bag (graph)
+    public Graph(DataInputStream inputData) throws IOException {
+        this(inputData.readInt()); //Reads number of Vertices (DataStream in) and construct this graph
+        int numberOfEdges= inputData.readInt();//numberOfEdges
+        for (int i =0;i<numberOfEdges;i++){
+            int firstVertex= inputData.readInt();    // firstVertex or location of the the list in the graph
+            int secondVertex= inputData.readInt();   // secondVertex or location of the the list in the graph
+            addEdge(firstVertex,secondVertex);   //adds the data inputs to the addEdge method and creates an edge between eage node.
 
         }
 
@@ -30,15 +34,17 @@ public class Graph {
     public int v(){
         return numberOfVertices;
     }
+
     public int E(){
         return numberOfEdges;
     }
-    public void  addEdge(int v, int w){
-        adjacentLists[v].add(w);
-        adjacentLists[w].add(w);
-        numberOfEdges++;
+
+    public void  addEdge(int firstVertex, int secondVertex){
+        adjacentLists[firstVertex].add(secondVertex);    //connects second vertex to first list
+        adjacentLists[secondVertex].add(firstVertex);   //connects first vertex to second list
+        numberOfEdges++;    //increase the edges made.
     }
-    public  Iterable<Integer> adj(int numberOfVertices){
-        return adj(numberOfVertices);
+    public  Iterable<Integer> adj(int vertex){
+        return adj(vertex);
     }
 }
--- Algorithms/src/main/java/practiceFolder/GraphingAlgorithmsPractice/DepthFirstPathsPractice.java
+++ Algorithms/src/main/java/practiceFolder/GraphingAlgorithmsPractice/DepthFirstPathsPractice.java
@@ -6,35 +6,40 @@ import  java.util.Stack;
 public class DepthFirstPathsPractice {
     private boolean[] marked;   //has dfs method been called for this vertex
     private int[] edgeTo;   //Last vertex on known path to this vertex
-    private final int source;   //Source is the starting vertex
+    private final int source;   //Source is the is the vertex your trying to find
 
     //
     public DepthFirstPathsPractice(Graph graph,int source){
         marked=new boolean[graph.v()];  //makes a new boolean vertex in the array
         edgeTo=new int[graph.E()];//this puts the number of edges in the array
-        this.source=source; //
-        dfs(graph,source);
+        this.source=source; //the vertex
+        dfs(graph,source);  //calls the dfs method to
 
     }
+    //checks if the node and edge have been checked.
     private  void  dfs(Graph graph, int vertex){
         marked[vertex]=true;//The boolean in the marked array is set to true
-        for (int w: graph.adj(vertex)){ //enhanced for loop that searches for in adjacent lists
-            if(!marked[w]){
-                edgeTo[w]=vertex;
-                dfs(graph,w);
+        for (int vertexLocation: graph.adj(vertex)){ //enhanced for loop that searches for in adjacent lists
+            if(!marked[vertexLocation]){ //if dfs has not marked this vertex true.
+                edgeTo[vertexLocation]=vertex;  //It adds the vertex and make a edge connectino
+                dfs(graph,vertexLocation);  //recursive call that checks vertex to true and checks if vertex is in
             }
         }
     }
+    //checks if the vertex is marked and true.
+    // This just means that it makes a edge.
     public boolean hasPathTo(int v){
         return marked[v];
     }
-    public Iterable<Integer>pathTo(int v){
-        if(!hasPathTo(v)){
+
+    public Iterable<Integer>pathTo(int vertex){
+        if(!hasPathTo(vertex)){
             return null;
         }
+        //makes a stack that searches for the vertex from the source(its the one we are looking for)
         Stack<Integer>path=new Stack<Integer>();
-        for(int x=v; x!=source;x=edgeTo[x]){
-            path.push(source);
+        for(int vertexLocation=vertex; vertexLocation!=source;vertexLocation=edgeTo[vertexLocation]){
+            path.push(vertexLocation);
         }
         path.push(source);
         return path;
